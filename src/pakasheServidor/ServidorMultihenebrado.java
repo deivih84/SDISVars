d971f74cd27a761569761086027dfc6cdef2ab2e
deivih84 <108626542+deivih84@users.noreply.github.com>
2024-02-26 09:59:06 +0100

    Primer commit para el yit

---
+++ src/pakasheServidor/ServidorMultihenebrado.java
@@ -0,0 +1,41 @@
+package pakasheServidor;
+
+import java.io.IOException;
+
+public class ServidorMultihenebrado {
+    public static final int PUERTO = 2000;
+
+    public static void main(String[] args) throws IOException {
+        try (java.net.ServerSocket servidor = new java.net.ServerSocket(PUERTO)) {
+            while (true) {
+                try {
+                    System.out.println("----Servidor esperando al cliente ----");
+                    java.net.Socket sock = servidor.accept(); // ojito ! sin try-with-rc
+                    java.io.BufferedReader inred = new java.io.BufferedReader(new java.io.InputStreamReader(sock.getInputStream()));
+                    java.io.PrintStream outred =
+                            new java.io.PrintStream(sock.getOutputStream());
+                    Runnable sirviente = () -> {
+                        try {
+                            String linea;
+                            while ((linea = inred.readLine()) != null) {
+                                System.out.println("Echoing: " + linea);
+                                outred.println(linea);
+                            }
+                        } catch (java.io.IOException ioe) {
+                            System.err.println("Cerrando socket de cliente");
+                            ioe.printStackTrace(System.err);
+                        }
+                    };
+                    Thread t = new Thread(sirviente, "Sirviente echo");
+                    t.start();
+                } catch (java.io.IOException e) {
+                    System.err.println("Cerrando socket de cliente");
+                    e.printStackTrace(System.err);
+                }
+            } // fin del while()
+        } catch (java.io.IOException e) {
+            System.err.println("Cerrando socket de servicio");
+            e.printStackTrace(System.err);
+        }
+    }
+}
